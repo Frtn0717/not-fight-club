61258667173015036c83d12dc0ace2eeae6da8b9
TL2018 <pontty232@gmail.com>
2025-08-22 15:19:17 +0300

    feat: add avatar selection functionality

--- pages/profile/script.js
+++ pages/profile/script.js
@@ -6,6 +6,7 @@ const playerAvatar = localStorage.getItem("player/avatar");
 const main = document.querySelector("main");
 const statistics = document.querySelector(".statistics");
 const avatarBlock = document.querySelector(".avatar");
+const avatarEditButton = document.querySelector(".avatar__edit_btn");
 
 const h1 = document.createElement("h1");
 h1.className = "playerName";
@@ -29,3 +30,46 @@ avatarImg.setAttribute("src", `/assets/avatars/player/${playerAvatar}.png`);
 avatarImg.setAttribute("alt", "Player avatar");
 
 avatarBlock.prepend(avatarImg);
+
+const avatarsContainer = document.createElement("div");
+avatarsContainer.className = "hide";
+
+const closeButton = document.createElement("div");
+closeButton.className = "close-button";
+
+const closeButtonIcon = document.createElement("img");
+closeButtonIcon.setAttribute("src", "/assets/icons/close.png");
+closeButtonIcon.setAttribute("alt", "Close button");
+closeButtonIcon.className = "close-button-icon";
+
+const avatars = ["human", "orc", "ne", "undead"];
+
+avatars.forEach((avatar) => {
+  const altAvatar = document.createElement("img");
+  altAvatar.setAttribute("src", `/assets/avatars/player/${avatar}.png`);
+  altAvatar.setAttribute("alt", "avatar");
+  altAvatar.setAttribute("name", avatar);
+  avatarsContainer.append(altAvatar);
+});
+
+main.append(avatarsContainer);
+
+avatarsContainer.append(closeButton);
+closeButton.append(closeButtonIcon);
+
+avatarEditButton.addEventListener("click", () => {
+  avatarsContainer.classList.remove("hide");
+  avatarsContainer.classList.add("alt_avatars_container");
+});
+
+closeButton.addEventListener("click", () => {
+  avatarsContainer.classList.remove("alt_avatars_container");
+  avatarsContainer.classList.add("hide");
+});
+
+avatarsContainer.addEventListener("click", (e) => {
+  e.target.name && localStorage.setItem("player/avatar", e.target.name);
+  avatarsContainer.classList.remove("alt_avatars_container");
+  avatarsContainer.classList.add("hide");
+  avatarImg.setAttribute("src", `/assets/avatars/player/${e.target.name}.png`);
+});
