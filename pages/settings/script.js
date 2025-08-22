a51fd4eb9a9d4ef8d4a3c92552627dd1593d63a8
TL2018 <pontty232@gmail.com>
2025-08-22 16:57:49 +0300

    feat: add change name functionality

--- pages/settings/script.js
+++ pages/settings/script.js
@@ -6,6 +6,8 @@ const container = document.createElement("div");
 const settingsField = document.createElement("p");
 const playerName = document.createElement("p");
 const editButton = document.createElement("input");
+const nameEnterField = document.createElement("input");
+const saveButton = document.createElement("input");
 
 container.className = "container";
 settingsField.className = "settings-field";
@@ -19,3 +21,25 @@ playerName.innerHTML = playerNameFromStorage;
 
 container.append(settingsField, playerName, editButton);
 main.append(container);
+
+nameEnterField.setAttribute("type", "text");
+nameEnterField.setAttribute("value", playerNameFromStorage);
+nameEnterField.className = "name-enter-field";
+saveButton.setAttribute("type", "button");
+saveButton.setAttribute("value", "Save");
+saveButton.className = "save-button";
+
+editButton.addEventListener("click", () => {
+  playerName.remove();
+  editButton.remove();
+  settingsField.after(nameEnterField, saveButton);
+});
+
+saveButton.addEventListener("click", () => {
+  const newPlayerName = nameEnterField.value;
+  nameEnterField.remove();
+  saveButton.remove();
+  settingsField.after(playerName, editButton);
+  playerName.innerHTML = newPlayerName;
+  localStorage.setItem("player/name", newPlayerName);
+});
